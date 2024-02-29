e6f50ba13d198a79f74b2daa7c4b697003566328
millatur <minnatur7373@gmail.com>
2024-02-29 23:55:40 +0600

    ed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,5 +20,11 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
